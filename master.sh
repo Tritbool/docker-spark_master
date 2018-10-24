6aa756b7d71a79d6e0cddb886786c81d799ff095
glyan <gauthier.lyan@gmail.com>
2018-10-24 10:31:02 +0200

    added /tmp/spark-events dir

--- master.sh
+++ master.sh
@@ -4,4 +4,6 @@ export SPARK_MASTER_HOST=`hostname`
 
 mkdir -p $SPARK_MASTER_LOG
 
+mkdir /tmp/spark-events
+
 spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port 7077 --webui-port 8080 >> $SPARK_MASTER_LOG/spark-master.out
